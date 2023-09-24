1e54fb0cb48b1ca30555ae95227f5a02144b0498
fernando-henrique <fernandohsferreira@gmail.com>
2023-09-24 17:20:38 -0400

    ok

--- src/js/logo.js
+++ src/js/logo.js
@@ -1,5 +1,8 @@
-if (window.matchMedia("(max-width: 900px)").matches) {
-    document.getElementById("logo").setAttribute("src", "src/imagens/logo.png")
-} else {
-    document.getElementById("logo").setAttribute("src", "src/imagens/logoEscritaLateral.png")
+function mudarLogo(){
+    if (window.matchMedia("(max-width: 900px)").matches) {
+        document.getElementById("logo").setAttribute("src", "src/imagens/logo.png")
+    } else {
+        document.getElementById("logo").setAttribute("src", "src/imagens/logoEscritaLateral.png")
+    }
 }
+mudarLogo()
